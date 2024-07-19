d1b66319397e3a7ad07e634523b609efaf7ffb45
Vlad-Petruk <vladpetruk00@gmail.com>
2024-07-19 12:12:29 +0300

    add cards and styling to the starting screen

---
+++ src/components/startingScreen/HouseCard.jsx
@@ -0,0 +1,10 @@
+
+function HouseCard({src, handleClick}) {
+    return (
+        <div className="card" onClick={handleClick}>
+            <img src= {src} alt={src} />
+        </div>
+    )
+}
+
+export default HouseCard
--- src/components/startingScreen/StartingScreen.jsx
+++ src/components/startingScreen/StartingScreen.jsx
@@ -2,6 +2,7 @@ import './StartingScreen.css'
 import HouseCard from './HouseCard'
 
 function StartingScreen () {
+    
 
     return (
         <div className='main-container'>
@@ -11,14 +12,14 @@ function StartingScreen () {
 
             <div className='housecards-box'>
                 <div className='info-box'>
-                    <p>You are expected to use your memory to help fighting against Voldemort </p>
+                    <p>You are expected to use your memory to help fighting against Voldemort</p>
                     <p>Select your Hogwarts house</p>
                 </div>
                 <div className='housecards'>
-                    <HouseCard />
-                    <HouseCard />
-                    <HouseCard />
-                    <HouseCard />
+                    <HouseCard src= '/gryffindor-button.png'/>
+                    <HouseCard src= '/slytherin-button.png'/>
+                    <HouseCard src= '/hufflepuff-button.png'/>
+                    <HouseCard src= '/ravenclaw-button.png'/>
                 </div>
             </div>
             
