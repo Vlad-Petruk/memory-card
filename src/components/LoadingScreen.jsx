a4e990da0478ee7b19e7ad3bcc2d531d5d9b2830
Vlad-Petruk <vladpetruk00@gmail.com>
2024-07-24 18:11:59 +0300

    add Loadng screen(commented)

---
+++ src/components/LoadingScreen.jsx
@@ -0,0 +1,10 @@
+import './LoadingScreen.css'
+function LoadingScreen ({house}) {
+    return (
+        <div className={`loading ${house}`}>
+            {/* Loading... */}
+        </div>
+    )
+}
+
+export default LoadingScreen
