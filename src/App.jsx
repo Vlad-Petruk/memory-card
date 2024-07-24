0e2d24a5be325975d32974b8e8432b813e18a344
Vlad-Petruk <vladpetruk00@gmail.com>
2024-07-24 17:32:41 +0300

    add audio playing(disabled)

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
-import { useState } from "react";
+import { useState} from "react";
 import "./App.css";
 import StartingScreen from "./components/startingScreen/StartingScreen";
 import Game from "./components/game/Game";
+import sound from './assets/sound.mp3'
 
-//characters in the api call can be saved with useMemo()???
+const audio = new Audio(sound);
+audio.volume = 0.2;
 
 function App() {
   const [gameOn, setGameOn] = useState(false);
-  const [house, setHouse] = useState(null)
+  const [house, setHouse] = useState(null);
 
+    const playAudio = () => {
+      audio.currentTime = 0;
+      audio.play();
+    };
+
+    // playAudio();
   return (
     <>
       {gameOn ? <Game house={house}/> : <StartingScreen handleClick={() => setGameOn(true)} handleHouseClick={(selectedHouse) => setHouse(selectedHouse)} /> }
